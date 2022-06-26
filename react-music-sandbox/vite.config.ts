2295d65e6b6c221dee77644d133ce7f66bc54099
RoneLoreto <32950316+RoneLoreto@users.noreply.github.com>
2022-06-27 00:42:21 +0900

    init

---
+++ react-music-sandbox/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  root: './src',
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: '../public',
+    emptyOutDir: true,
+  },
+  plugins: [react()],
+});
